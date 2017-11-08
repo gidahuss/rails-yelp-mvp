deb5a484ad6d8a1133de2bb69e261ed982315ccf
gida huss <g_hussami@hotmail.com>
2017-11-08 17:33:01 +0100

    restaurants & so

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+root to: 'restaurants#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
 end
+
+
